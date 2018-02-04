5dd32841cb7509d4c3fbcf4420bae3e896299d20
xmd5a <zakaru@gmail.com>
2018-02-04 13:20:53 +0100

    frontend scrollbar files

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,11 @@ class WordpressReadingProgress {
         this.element = document.getElementById(element);
         this.screenHeight = this.getScreenHeight();
         this.elementOffset = this.getElementOffset();
+        this.scrollPosition = this.getScrollPosition();
+        this.scrollbarElement = null;
+        this.scrollbarElementID = 'wordpress-reading-progress-bar';
+
+        this.appendScrollbarElement(document.getElementsByTagName('body')[0]);
 
         window.addEventListener('resize', this.checkReadingProgress.bind(this));
         window.addEventListener('scroll', this.checkReadingProgress.bind(this));
@@ -17,9 +22,30 @@ class WordpressReadingProgress {
         return Math.max(document.documentElement.clientHeight, window.innerHeight);
     }
 
+    getScrollPosition() {
+        return document.documentElement.scrollTop;
+    }
+
+    appendScrollbarElement(appendTo) {
+        this.scrollbarElement = document.createElement('div');
+        this.scrollbarElement.setAttribute('id', this.scrollbarElementID);
+        appendTo.appendChild(this.scrollbarElement);
+        this.scrollbarElement.appendChild(document.createElement('div'));
+    }
+
     checkReadingProgress() {
         this.elementOffset = this.getElementOffset();
         this.screenHeight = this.getScreenHeight();
+        this.scrollPosition = this.getScrollPosition();
+
+        const currentOffset = this.elementOffset - this.screenHeight;
+        const totalOffset = this.scrollPosition + currentOffset;
+        let currentPercentPosition = Math.abs((currentOffset * 100 / totalOffset) - 100);
+
+        if(currentPercentPosition > 100)
+            currentPercentPosition = 100;
+
+        document.querySelector(`#${this.scrollbarElementID} > div`).style.width = currentPercentPosition + '%';
     }
 }
 
